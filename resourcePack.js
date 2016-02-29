f8dc426e9fc07354c5d3da915ea65715507f2f24
Satoru Nakata <nakata0705@gmail.com>
2016-02-28 20:09:36 -0800

    Updated how img tag loads textures.

--- resourcePack.js
+++ resourcePack.js
@@ -13,34 +13,36 @@ pc.script.create('mineCraftResourece', function (app) {
         // Called once after all resources are loaded and before the first update
         initialize: function () {
 			var self = this;
-			self.$canvas = $('<canvas width=4096 height=4096>');
+			self.$canvas = $('<canvas class="texture" width=512 height=512>');
+			self.$canvas.css({ "position": "absolute", "top": 128, "left": 0, "z-index": 100 });
 			self.context2D = this.$canvas.get(0).getContext("2d");
-			self.context2D.fillStyle = "rgb(255, 255, 255)";
-			self.context2D.fillRect(0, 0, this.$canvas.width(), this.$canvas.height());
     		$('body').append(self.$canvas);
     		
     		self.$img = [];
-    		self.$img[0] = $('<img src="files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/stone.png">');
-    		self.$img[1] = $('<img src="files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/grass_side.png">');
-    		self.$img[2] = $('<img src="files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/dirt.png">');
-    		self.$img[3] = $('<img src="files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/cobblestone.png">');
-    		self.$img[0].ready(function () {
-				self.context2D.drawImage(self.$img[0].get(0), 32, 0);    			
-				console.log("ResourcePack");
-    		});
-    		self.$img[1].ready(function () {
-				self.context2D.drawImage(self.$img[1].get(0), 32 * 2, 0);    			
-				console.log("ResourcePack");
-    		});
-    		self.$img[2].ready(function () {
-				self.context2D.drawImage(self.$img[2].get(0), 32 * 3, 0);    			
-				console.log("ResourcePack");
-    		});
-    		self.$img[3].ready(function () {
-				self.context2D.drawImage(self.$img[3].get(0), 32 * 4, 0);    			
-				console.log("ResourcePack");
-    		});
-    		
+    		$("<img src='files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/stone.png'>").one("load", function() {
+					self.context2D.drawImage(this, 32, 0);    			
+					console.log("ResourcePack");
+    			}).each(function() {
+    				if(this.complete) $(this).load();
+    			});
+    		$("<img src='files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/grass_side.png'>").one("load", function() {
+					self.context2D.drawImage(this, 64, 0);    			
+					console.log("ResourcePack");
+    			}).each(function() {
+    				if(this.complete) $(this).load();
+    			});
+    		$("<img src='files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/dirt.png'>").one("load", function() {
+					self.context2D.drawImage(this, 96, 0);    			
+					console.log("ResourcePack");
+    			}).each(function() {
+    				if(this.complete) $(this).load();
+    			});
+    		$("<img src='files/resourcePack/John Smith Legacy 1.8.9 v1.3.26/assets/minecraft/textures/blocks/cobblestone.png'>").one("load", function() {
+					self.context2D.drawImage(this, 128, 0);    			
+					console.log("ResourcePack");
+    			}).each(function() {
+    				if(this.complete) $(this).load();
+    			});
 		},
     };
 
